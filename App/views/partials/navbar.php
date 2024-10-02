2b7b4898c9aa48a820035c867d11770d58861dac
TahirUddinAhmed <01tahirahmed@gmail.com>
2024-10-02 12:36:50 +0530

    Create navbar dynamic so that login user can see logout button and post a job

--- App/views/partials/navbar.php
+++ App/views/partials/navbar.php
@@ -1,3 +1,8 @@
+<?php 
+
+use Framework\Session;
+
+?>
     <!-- Nav -->
     <header class="bg-blue-900 text-white p-4">
       <div class="container mx-auto flex justify-between items-center">
@@ -5,13 +10,27 @@
           <a href="/">Work Hunt</a>
         </h1>
         <nav class="space-x-4">
-          <a href="/auth/login" class="text-white hover:underline">Login</a>
-          <a href="/auth/register" class="text-white hover:underline">Register</a>
-          <a
-            href="/listings/create"
-            class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300"
-            ><i class="fa fa-edit"></i> Post a Job</a
-          >
+          <?php if(Session::has('user')) : ?>
+            <div class="flex justify-between items-center gap-4">
+              <div>
+                Welcome <?= Session::get('user')['name'] ?>
+              </div>
+              <form action="">
+                <button type="submit" class="text-white inline hover:underline">Logout</button>
+              </form>
+              <a
+                href="/listings/create"
+                class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300"
+                ><i class="fa fa-edit"></i> Post a Job</a>
+            </div>
+
+          <?php else : ?>
+
+            <a href="/auth/login" class="text-white hover:underline">Login</a>
+            <a href="/auth/register" class="text-white hover:underline">Register</a>
+          
+          <?php endif; ?>
+
         </nav>
       </div>
     </header>
